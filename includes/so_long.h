1badc18d796f88ec9a483f5a907e048d7ba25b68
Alisa Tonkopiy <atonkopi@bess-f4r3s2.clusters.42paris.fr>
2024-02-06 15:39:09 +0100

    added display and window init

--- includes/so_long.h
+++ includes/so_long.h
@@ -2,6 +2,8 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
-
+#include <../mlx/mlx.h>
+# define WINDOW_WIDTH 600
+# define WINDOW_HEIGHT 300
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include "../includes/so_long.h"
-#include <stdlib.h>
-#include <../mlx/mlx.h>
 
 int main(void)
 {
     void	*mlx_ptr;
+    void	*win_ptr;
 
     mlx_ptr = mlx_init();
+    if (!mlx_ptr)
+		return (1);
+    win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+    if (!win_ptr)
+		return (free(mlx_ptr), 1);
+    while (1)
+        ;
+    mlx_destroy_window(mlx_ptr, win_ptr);
     mlx_destroy_display(mlx_ptr);
     free(mlx_ptr);
+    return(0);
 }
